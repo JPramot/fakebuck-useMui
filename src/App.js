8ccb2dde420e65dd2b4a3117fb9c31874299cd74
JPramot <jkurathong@gmail.com>
2024-01-09 11:43:23 +0700

    createComponent

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import { Button, Box } from "@mui/material";
+import { Button, Box, Container } from "@mui/material";
+import Navbar from "./component/Navbar";
+import Sidebar from "./component/Sidebar";
+import Feed from "./component/Feed";
+import Rightbar from "./component/Rightbar";
 
 function App() {
   return (
-    <Box
-      component="main"
-      sx={{
-        p: 2,
-        border: "1px dashed grey",
-        "&:hover": {
-          bgcolor: "red",
-        },
-      }}
-      height={500}
-      width={200}
-      bgcolor="wheat"
-      my={4}
-      mx={4}
-      display="flex"
-      gap={4}
-      alignItems="center"
-    >
-      <Button variant="contained">Hello MUI</Button>
-      <Button variant="contained">Hello MUI</Button>
+    <Box>
+      <Navbar />
+      <Sidebar />
+      <Feed />
+      <Rightbar />
     </Box>
   );
 }
---
+++ src/learn/container.js
@@ -0,0 +1,12 @@
+import { Button, Box, Container } from "@mui/material";
+
+function App() {
+  return (
+    <Container maxWidth="sm" sx={{ bgcolor: "wheat", height: "100vh" }}>
+      <Button variant="contained">Hello MUI</Button>
+      <Button variant="contained">Hello MUI</Button>
+    </Container>
+  );
+}
+
+export default App;
